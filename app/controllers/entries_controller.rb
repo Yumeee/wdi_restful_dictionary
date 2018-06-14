c1ad356bf7e5373b2020e79979bc72e97067752e
YUMEE <yumeehk@hotmail.com>
2018-06-14 14:14:44 -0400

    update, edit added

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -3,10 +3,6 @@ class EntriesController < ApplicationController
     render :index
   end
 
-  def show
-    render :show
-  end
-
   def new
     render :new
   end
@@ -15,4 +11,11 @@ class EntriesController < ApplicationController
     redirect_to entries_url
   end
 
+  def edit
+    render :edit
+  end 
+
+  def update
+    redirect_to entry_url(params[:id])
+  end
 end
