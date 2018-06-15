7ddaa7e4332ca40643158dc3572cca5242cb3b4a
YUMEE <yumeehk@hotmail.com>
2018-06-15 16:05:46 -0400

    finished entries

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,22 +1,49 @@
 class EntriesController < ApplicationController
   def index
+    @entries = Entry.all
   end
 
   def new
+    @entry = Entry.new
   end
 
   def create
-    redirect_to entries_url
+    @entry = Entry.new
+    @entry.word = params[:entry][:word]
+    @entry.definition = params[:entry][:definition]
+    @entry.language = params[:entry][:language]
+
+    if @entry.save
+      redirect_to entries_url
+    else
+      redirect_to new_entry_url
+    end
+  end
+
+  def show
+    @entry = Entry.find_by(id: params[:id])
   end
 
   def edit
+    @entry = Entry.find_by(id: params[:id])
   end
 
   def update
-    redirect_to entry_url(params[:id])
+    @entry = Entry.find_by(params[:id])
+    @entry.word = params[:entry][:word]
+    @entry.definition = params[:entry][:definition]
+    @entry.language = params[:entry][:language]
+
+    if @entry.save
+      redirect_to entry_url(@entry)
+    else
+      redirect_to entry_url(@entry)
+    end
   end
 
   def destroy
-    redirect_to index_url
+    @entry = Entry.find(params[:id])
+    @entry.destroy
+    redirect_to entries_path
   end
 end
